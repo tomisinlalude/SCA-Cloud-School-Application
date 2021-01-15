3a022a5422d4ad2272de8e52750fcd40b02a5f32
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-15 13:26:52 +0100

    Update shell script

--- os-cli.sh
+++ os-cli.sh
@@ -6,8 +6,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	echo "************************************************************************"
 	echo "Checking for WGET package"
 	if ! which wget > /dev/null; then
-  	   echo "Command not found! Installing"
-     	   sudo apt-get install wget
+  	   echo "Package not found! Installing"
+     	   sudo apt-get install -y wget
 	   echo "wget has been installed"
 	else
 	   echo "Wget is already installed"
@@ -16,8 +16,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	echo "************************************************************************"
 	echo "Checking for CURL package"
 	if ! which curl > /dev/null; then
-  	   echo "Command not found! Installing"
-     	   sudo apt-get install curl
+  	   echo "Package not found! Installing"
+     	   sudo apt-get install -y curl
 	   echo "Curl has been installed"
 	else
 	   echo "curl is already installed"
@@ -26,8 +26,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	echo "************************************************************************"
 	echo "Checking for Node.js package"
 	if ! which node > /dev/null; then
-  	   echo "Command not found! Installing"
-     	   sudo apt-get install nodejs
+  	   echo "Package not found! Installing"
+     	   sudo apt-get install -y nodejs
 	   echo "Node.js has been installed"
 	else
 	   echo "Node.js is already installed"
@@ -35,14 +35,80 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "This is a darwin OS"
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-        echo "This is a cygwin OS"
-elif [[ "$OSTYPE" == "msys" ]]; then
-        echo "This is a msys OS"
-elif [[ "$OSTYPE" == "win32" ]]; then
-        echo "This is a win32 OS"
+	echo "************************************************************************"
+	echo "Checking for WGET package"
+	if ! which wget > /dev/null; then
+  	   echo "Package not found! Installing"
+     	   sudo brew install -y wget
+	   echo "wget has been installed"
+	else
+	   echo "Wget is already installed"
+	fi
+
+	echo "************************************************************************"
+	echo "Checking for CURL package"
+	if ! which curl > /dev/null; then
+  	   echo "Package not found! Installing"
+     	   sudo brew install -y curl
+	   echo "Curl has been installed"
+	else
+	   echo "curl is already installed"
+	fi
+
+	echo "************************************************************************"
+	echo "Checking for Node.js package"
+	if ! which node > /dev/null; then
+  	   echo "Package not found! Installing"
+     	   sudo brew install -y node
+	   echo "Node.js has been installed"
+	else
+	   echo "Node.js is already installed"
+	fi
+
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
         echo "This is a freebsd OS"
+	echo "************************************************************************"
+	echo "Checking for WGET package"
+	if ! which wget > /dev/null; then
+  	   echo "Package not found! Installing"
+     	   sudo pkg install -y wget
+	   sudo rehash
+	   echo "wget has been installed"
+	else
+	   echo "Wget is already installed"
+	fi
+
+	echo "************************************************************************"
+	echo "Checking for CURL package"
+	if ! which curl > /dev/null; then
+  	   echo "Package not found! Installing"
+     	   sudo pkg install -y curl
+	   sudo rehash
+	   echo "Curl has been installed"
+	else
+	   echo "curl is already installed"
+	fi
+
+	echo "************************************************************************"
+	echo "Checking for Node.js package"
+	if ! which node > /dev/null; then
+  	   echo "Package not found! Installing"
+     	   sudo pkg install -y node12
+	   sudo rehash
+	   echo "Node.js has been installed"
+           
+	else
+	   echo "Node.js is already installed"
+	fi
+
 else
         echo "Installation for your OS is not available"
 fi
+
+#elif [[ "$OSTYPE" == "cygwin" ]]; then
+#        echo "This is a cygwin OS"
+
+#elif [[ "$OSTYPE" == "msys" ]]; then
+#        echo "This is a msys OS"
+#elif [[ "$OSTYPE" == "win32" ]]; then
+#        echo "This is a win32 OS"
